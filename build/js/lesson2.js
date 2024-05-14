4603a714be5df4f26e1bbe439a1d9c2b7969852c
KunalWebDevMern <kodshroud@gmail.com>
2024-05-15 02:14:11 +0530

    Added some files

--- build/js/lesson2.js
+++ build/js/lesson2.js
@@ -2,3 +2,16 @@
 let myName = "Kunal";
 let meanoflife;
 let isLoading;
+// let album:any;
+let album;
+myName = "Ishwar";
+meanoflife = 23;
+isLoading = true;
+// album = 1098;
+album = "Hi there";
+const sum = (a, b) => {
+    return a + b;
+};
+let postId;
+let isActive;
+let re = /\w+/g;
--- src/lesson2.ts
+++ src/lesson2.ts
@@ -1,3 +1,21 @@
 let myName: string = "Kunal";
 let meanoflife:number ;
 let isLoading:boolean;
+// let album:any;
+
+let album: string | number ;
+
+myName = "Ishwar";
+meanoflife = 23;
+isLoading = true;
+// album = 1098;
+album = "Hi there";
+
+const sum = (a:number,b:number)=>{
+    return a + b;
+}
+
+let postId: string | number;
+let isActive: boolean | number;
+
+let re: RegExp = /\w+/g;
